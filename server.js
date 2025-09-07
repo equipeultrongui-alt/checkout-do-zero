cd0a519fc2983f50c0bc0d0fa1492ee91632c0b7
equipeultrongui-alt <equipeultrongui@gmail.com>
2025-09-07 02:03:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir tudo que estiver dentro da pasta public/
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
 
